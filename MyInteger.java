d44e846ff4c07b329a85d564fe46bc4d41dae6ac
linh8911nt <thanhlinh12314@gmail.com>
2018-03-31 11:55:39 +0700

    MyInteger class and its test class

--- MyInteger.java
+++ MyInteger.java
@@ -1,2 +1,95 @@
+import java.util.Objects;
+
 public class MyInteger {
+    private int value;
+
+    public MyInteger() {
+    }
+
+    public MyInteger(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    //số chẵn
+    public boolean isEven(){
+        if (value % 2 == 0){
+            return true;
+        } else
+            return false;
+    }
+
+    //số lẻ
+    public boolean isOdd(){
+        if (value % 2 != 0)
+            return false;
+        else
+            return true;
+    }
+
+    //số nguyên tố
+    public boolean isPrime(){
+        if (value < 2) {
+            return false;
+        } else {
+            int i = 2;
+            boolean check = true;
+            while (i <= Math.sqrt(value)) {
+                if (value % i == 0) {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    //số chẵn
+    public static boolean isEven(int value){
+        if (value % 2 == 0){
+            return true;
+        } else
+            return false;
+    }
+
+    //số lẻ
+    public static boolean isOdd(int value){
+        if (value % 2 != 0)
+            return false;
+        else
+            return true;
+    }
+
+    //số nguyên tố
+    public static boolean isPrime(int value){
+        if (value < 2) {
+            return false;
+        } else {
+            int i = 2;
+            boolean check = true;
+            while (i <= Math.sqrt(value)) {
+                if (value % i == 0) {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check)
+                return true;
+            else
+                return false;
+        }
+    }
+
 }
--- MyIntegerTest.java
+++ MyIntegerTest.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class MyIntegerTest {
     public static void main(String[] args) {
         MyInteger myInterger = new MyInteger();
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter value: ");
+        int value = input.nextInt();
     }
 }
